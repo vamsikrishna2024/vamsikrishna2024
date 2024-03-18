11eefddea3fa76870c763daf2c91a65e1fa25bfe
hi <yvamsikrishna2022@gmail.com>
2024-03-18 13:06:06 +0530

    forloop

---
+++ src/main/scala/scala_forloop.scala
@@ -0,0 +1,37 @@
+object scala_forloop {
+  def main(ards: Array[String]): Unit ={
+  /**  var a = 9
+
+    if(a >0 && a% 2== 0)
+      {
+        print("even ")
+
+      }
+    else
+      {
+        println("odd")
+      }**/
+    val a = 9
+    val b =11
+    if (a >10 || b < 10) {
+
+      print(" gaterthen 10")
+
+    }
+        else
+        {
+          print(" less then 10")
+        }
+      }
+  val age = 25;
+  def isEligible(a:Int) = age<=a;
+  {
+    var a = scala.io.StdIn.readInt()
+    if(isEligible(a))
+      println("Person is eligible")
+    else
+      println("Person is ineligible")
+  }
+
+
+}
